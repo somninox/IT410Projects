a826c0abdcc6c0eba3a477d7507b766b23ddf395
wjwl <whitneywinders@live.com>
2016-02-24 18:19:25 -0700

    updated angular test example

--- angular/app.js
+++ angular/app.js
@@ -6,24 +6,33 @@
 
 		this.guess = 50;
 
+		this.guesses = [];
+
 		this.remaining = 7;
 
 		this.message = '';
+
+		this.reveal;
 
 		this.checkGuess = function(e) {
 			e.preventDefault();
 			this.remaining--;
 			if(this.guess == answer){
-				this.message = 'You are so right';
+				this.message = 'You are so right!';
 				this.done = true;
 			}
 			else if(this.guess < answer){
-				this.message = "guess higher";
+				this.message = "Guess higher";
 			}
 			else if(this.guess > answer){
 				this.message = "Guess lower";
 			}
-			if(this.remaining === 0) this.done = true;
+			if(this.remaining === 0){
+				this.reveal = answer;
+				this.done = true;
+			}
+
+			this.guesses.push(this.guess);
 			
 		}
 	}])
