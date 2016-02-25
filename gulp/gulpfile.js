72a77b3015940839ffbc53d6d026ff1353c5b4e7
wjwl <whitneywinders@live.com>
2016-02-24 17:24:57 -0700

    updated gulpfile with gulp-changed

--- gulp/gulpfile.js
+++ gulp/gulpfile.js
@@ -7,6 +7,7 @@ var htmlmin 		= require('gulp-htmlmin');
 var imagemin  		= require('gulp-imagemin');
 var uglify 			= require('gulp-uglify');
 var useref  		= require('gulp-useref');
+var changed 		= require('gulp-changed');
 
 gulp.task('hello', function(){
 	console.log('Hello Whitney');
@@ -14,6 +15,7 @@ gulp.task('hello', function(){
 
 gulp.task('sass', function(){
  return gulp.src('src/scss/**/*.scss')
+ 	.pipe(changed('app/css'))
 	.pipe(sass())
 	.pipe(autoprefixer('last 2 version'))
 	.pipe(csso())
@@ -23,6 +25,7 @@ gulp.task('sass', function(){
 
 gulp.task('css', function(){
  return gulp.src('src/css/**/*.css')
+ 	.pipe(changed('app/css'))
 	.pipe(autoprefixer('last 2 version'))
 	.pipe(csso())
 	.pipe(useref())
@@ -31,12 +34,14 @@ gulp.task('css', function(){
 
 gulp.task('htmlmin', function(){
 	return gulp.src('src/html/**/*.html')
+		.pipe(changed('app/html'))
 		.pipe(htmlmin({collapseWhitespace: true}))
 		.pipe(gulp.dest('app/html'));
 });
 
 gulp.task('imagemin', () => {
 	return gulp.src('src/images/**/*')
+		.pipe(changed('app/images'))
 		.pipe(imagemin({
 			progressive: true,
 			svgoPlugins: [{removeViewBox: false}],
@@ -46,6 +51,7 @@ gulp.task('imagemin', () => {
 
 gulp.task('java', function(){
 	return gulp.src('src/javascript/**/*.js')
+		.pipe(changed('app/javascript'))
 		.pipe(uglify()).on('error', function(e){
             console.log(e);
          })
